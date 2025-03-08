81fc858d118d608edb1bd961b1fb7cd064660ffd
dvlt-org <dvltinv@gmail.com>
2025-03-08 04:34:20 +0300

    latest

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   webpack(config, { isServer }) {
+    // Serverda ishlashda cache'ni o'chirib qo'yish
     if (!isServer) {
       config.cache = false;
     }
 
+    // Qo'shimcha webpack konfiguratsiyalari kerak bo'lsa, bu yerga qo'shishingiz mumkin
     return config;
   },
   reactStrictMode: true,
-  swcMinify: true,
-  output: "export"
+  swcMinify: true
+  // Qo'shimcha Next.js konfiguratsiyalari bu yerda bo'lishi mumkin
 };
 
 export default nextConfig;
